c3ec3776cc8ad5074a801d403dd589131866760f
mbahArip <main@mbaharip.me>
2021-08-25 21:59:52 +0700

    pagination added

--- assets/js/func.js
+++ assets/js/func.js
@@ -111,9 +111,9 @@ class googleFirebase {
 	}
 
 	getGallery(query) {
-		firebase.database().ref('portofolio')
+		firebase.database().ref('resource')
 			.orderByChild('id')
-			.limitToLast(12)
+			.limitToLast(5)
 			.get()
 			.then(async snapshot => {
 				let items = Object.values(snapshot.val());
@@ -181,7 +181,7 @@ class googleFirebase {
 			<img src="https://files.mbaharip.me/0:/posts/[${itemsList[`id`]}] ${itemsList['title']}/${itemsList['thumb']}" alt="${itemsList['title']}" class="thumb">
 			<div class="top">
 				<span class="title">
-					${itemsList['title']}
+					${itemsList['titleRomaji']}
 				</span>
 				<span class="description">
 					${itemsList['description']}
@@ -196,6 +196,181 @@ class googleFirebase {
 		document.querySelector('section#worksGallery div.container div.content').appendChild(post);
 		await _utility.sleep(10);
 		post.className = 'posts';
+	}
+	async pagination(query) {
+		function postData(itemsList, category, date) {
+			let post =
+				`
+				<img src="https://files.mbaharip.me/0:/posts/[${itemsList[`id`]}] ${itemsList['title']}/${itemsList['thumb']}" alt="${itemsList['title']}" class="thumb">
+				<div class="top">
+					<span class="title">
+						${itemsList['title']}
+					</span>
+					<span class="description">
+						${itemsList['description']}
+					</span>
+				</div>
+				<div class="bottom">
+					<span class="category">${category}</span>
+					<span class="date">${date}</span>
+				</div>
+				`;
+			return post;
+		}
+		let itemsPerPage = 5;
+		let currentPage;
+
+		let data = new Promise((resolve, reject) => {
+			try {
+				firebase.database().ref('portofolio')
+					.orderByChild('id')
+					.get()
+					.then(snapshot => {
+						let items = Object.values(snapshot.val());
+						resolve(items);
+					})
+			} catch (error) {
+				reject(error);
+			}
+		})
+
+		//Element
+		const contentWrapper = document.querySelector('section#worksGallery div.container div.content')
+		const paginationWrapper = document.querySelector('section#worksGallery div.container div.pagination')
+
+		//Get data
+		let items = await data;
+		let sortedItems = items.sort((a, b) => {
+			return b.id - a.id;
+		})
+		let filteredItems;
+		if (query != undefined) {
+			filteredItems = sortedItems.filter((item) => {
+				return item.category == query;
+			});
+		} else {
+			filteredItems = sortedItems;
+		}
+
+		//Page query
+		if (_utility.getParameter('page') == undefined || _utility.getParameter('page') == '') {
+			currentPage = 1;
+		} else {
+			currentPage = _utility.getParameter('page');
+		}
+
+		function displayItems(items, wrapper, itemsPerPage, page) {
+			wrapper.innerHTML = '';
+			page--;
+
+			let start = itemsPerPage * page;
+			let end = start + itemsPerPage;
+			let paginatedItems = items.slice(start, end);
+
+			for (let i = 0; i < paginatedItems.length; i++) {
+				let item = paginatedItems[i];
+
+				let category;
+				switch (item['category']) {
+					case '3d':
+						category = '3D Works'
+						break;
+					case 'livery':
+						category = 'Livery Design'
+						break;
+					case '2d':
+						category = '2D Graphics'
+						break;
+					case 'web':
+						category = 'Website'
+						break;
+				}
+				let date;
+				const monthNames = ["January", "February", "March", "April", "May", "June",
+					"July", "August", "September", "October", "November", "December"
+				];
+				let unix = new Date(item['date'] * 1000);
+				let dDate = unix.getDate();
+				let dMonth = monthNames[unix.getMonth()];
+				let dYear = unix.getFullYear();
+				date = `${dDate} ${dMonth} ${dYear}`;
+
+				let itemElement = document.createElement('a');
+				itemElement.href = `/works/details?id=${item['id']}`;
+				itemElement.className = 'posts';
+				itemElement.innerHTML = postData(item, category, date);
+
+				wrapper.appendChild(itemElement);
+			}
+		}
+
+		function paginationSetup(items, wrapper, itemsPerPage) {
+			wrapper.innerHTML = '';
+
+			let pageCount = Math.ceil(items.length / itemsPerPage);
+			for (let i = 1; i < pageCount + 1; i++) {
+				let btn = paginationBtn(i);
+				wrapper.appendChild(btn);
+			}
+		}
+
+		function paginationBtn(page) {
+			let button = document.createElement('span');
+			button.innerText = page;
+			button.className = 'page';
+
+			if (currentPage == page) button.className = 'page active';
+
+			button.addEventListener('click', async () => {
+				currentPage = page;
+				document.querySelector('div.container div.content').className = 'content hide';
+				await _utility.sleep(500);
+				displayItems(filteredItems, contentWrapper, itemsPerPage, currentPage);
+				paginationChangeURL(currentPage);
+				document.querySelector('div.container div.content').className = 'content';
+
+
+				let currentBtn = document.querySelector('span.page.active');
+				currentBtn.className = 'page';
+
+				button.className = 'page active';
+			})
+
+			return button;
+		}
+
+		function paginationChangeURL(page) {
+			let newURL = new URL(window.location.href);
+			let searchParameter = newURL.searchParams;
+			searchParameter.set(`page`, page);
+
+			newURL = newURL.toString();
+			let newTitle = `${document.title} - Page ${page}`;
+			let newState = {
+				additionalInformation: `Page ${page}`
+			};
+
+			window.history.replaceState(newState, newTitle, newURL);
+		}
+
+		if (filteredItems.length == 0) {
+			document.querySelector('div.container div.loading').classList = 'loading hide';
+			await _utility.sleep(500);
+			document.querySelector('div.container div.loading').style.display = 'none';
+			document.querySelector('div.container div.no_content').style.display = 'flex';
+			await _utility.sleep(10);
+			document.querySelector('div.container div.no_content').classList = 'no_content';
+		} else {
+			document.querySelector('div.container div.loading').classList = 'loading hide';
+			await _utility.sleep(500);
+			document.querySelector('div.container div.loading').style.display = 'none';
+			document.querySelector('div.container div.pagination').style.display = 'flex';
+			await _utility.sleep(10);
+			displayItems(filteredItems, contentWrapper, itemsPerPage, currentPage);
+			paginationSetup(filteredItems, paginationWrapper, itemsPerPage);
+			document.querySelector('div.container div.content').className = 'content';
+			document.querySelector('div.container div.pagination').className = 'pagination';
+		}
 	}
 }
 
@@ -269,5 +444,14 @@ class Util {
 
 	sleep(ms) {
 		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	getParameter(parameter) {
+		let baseURL = window.location.origin;
+		let parameterURL = window.location.search;
+		let url = new URL(parameterURL, baseURL);
+
+		let parameterValue = url.searchParams.get(parameter);
+		return parameterValue;
 	}
 }
