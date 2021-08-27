c4e4329bfd2c6e587c2f7a1119744cdb8d58d7ca
mbahArip <main@mbaharip.me>
2021-08-28 03:45:31 +0700

    update lil thing

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,17 +12,20 @@ async function global__init() {
 
 async function home__init() {
     global__init();
+    await _utility.sleep(10);
     _firebase.getWorks();
     _utility.footer();
 }
 
 async function works__init() {
     global__init();
+    await _utility.sleep(10);
     _utility.footer();
 }
 
 async function details__init() {
     global__init();
+    await _utility.sleep(10);
     _details.populateData();
     _utility.footer();
 }
